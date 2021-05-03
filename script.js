9a61772e3816039ae3e790226dd070de0039fd01
sardor4 <83544074+sardor4@users.noreply.github.com>
2021-05-04 00:28:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+var hour = +prompt('Введите время.');
+
+switch (hour) {
+    case 1:
+        alert('Час ночи.');
+        break;
+    case 2:
+        alert('2 часа ночи.');
+        break;
+    case 3:
+        alert('3 часа ночи.');
+        break;
+    case 4:
+        alert('4 часа ночи.');
+        break;
+    case 5:
+        alert('5 часа ночи.');
+        break;
+    case 6:
+        alert('6 часа утра.');
+        break;
+    case 7:
+        alert('7 часа утра.');
+        break;
+    case 8:
+        alert('8 часа утра.');
+        break;
+    case 9:
+        alert('9 часа утра.');
+        break;
+    case 10:
+        alert('10 часа утра.');
+        break;
+    case 11:
+        alert('11 часа утра.');
+        break;
+    case 12:
+        alert('12 часа дня.');
+        break;
+    case 13:
+        alert('13 часа дня.');
+        break;
+    case 14:
+        alert('14 часа дня.');
+        break;
+    case 15:
+        alert('15 часа дня.');
+        break;
+    case 16:
+        alert('16 часа вечера.');
+        break;
+    case 17:
+        alert('17 часа вечера.');
+        break;
+    case 18:
+        alert('18 часа вечера.');
+        break;
+    case 19:
+        alert('19 часа вечера.');
+        break;
+    case 20:
+        alert('20 часа вечера.');
+        break;
+    case 21:
+        alert('21 часа ночи.');
+        break;
+    case 22:
+        alert('22 часа ночи.');
+        break;
+    case 23:
+        alert('23 часа ночи.');
+        break;
+    case 24:
+        alert('24 часа ночи.');
+        break;
+    default:
+        alert('Ошибка.');
+        break;
+}
